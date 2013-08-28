2d97ec26edb05cb7f293b0357c1289653cf7d918
Josh Timmer <0ohexxo0@gmail.com>
2013-08-28 11:48:22 -0400

    Fixed tabs not being spaces

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,27 +3,27 @@ class Restaurant(object):
 
 class MenuItem(object):
     def __init__(self, name, description, price):
-	self.name = name
-	self.description = description
-	self.price = price
+        self.name = name
+        self.description = description
+        self.price = price
 
     def __str__(self):
-	return self.name
+        return self.name
 
     def __repr__(self):
-	return self.__str__()
+        return self.__str__()
 
 class LineItem(object):
     def __init__(self, item, quantity):
-	self.item = item
-	self.quantity = quantity
+        self.item = item
+        self.quantity = quantity
 
     def __str__(self):
-	return '{1}x {0}'.format(self.item, self.quantity)
+        return '{1}x {0}'.format(self.item, self.quantity)
 
     def __repr__(self):
-	return self.__str__()
+        return self.__str__()
 
 class Receipt(object):
     def __init__(self):
-	pass	
+        pass    

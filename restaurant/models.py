91f7a43d3582119d925c8f46b35d7080244b6f77
Jim Cushing <jimothy@mac.com>
2013-09-03 19:22:05 -0400

    started modeling a restaurant

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,2 +1,33 @@
-class Restaurant:
+class Restaurant(object):
     pass
+
+
+class Menu(object):
+    pass
+
+
+class Item(object):
+    def __init__(self, name, description, price):
+        self.name = name
+        self.description = description
+        self.price = price
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class Receipt(object):
+    def __init__(self):
+        pass
+
+
+class LineItem(object): 
+    def __init__(self, item, quantity):
+        self.item = item
+        self.quantity = quantity
+
+    def __str__(self):
+        return '{1} {0}(s)'.format(self.item, self.quantity) 

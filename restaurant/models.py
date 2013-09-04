48b1e9c22f3cc10357d1283bdad70d8722f2e1ce
InfinityTotality <0ohexxo0@gmail.com>
2013-09-03 21:12:43 -0400

    Expanded LineItem and Receipt

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,7 +1,12 @@
 class Restaurant(object):
     pass
 
-class MenuItem(object):
+
+class Menu(object):
+    pass
+
+
+class Item(object):
     def __init__(self, name, description, price):
         self.name = name
         self.description = description
@@ -13,17 +18,32 @@ class MenuItem(object):
     def __repr__(self):
         return self.__str__()
 
-class LineItem(object):
+
+class LineItem(object): 
     def __init__(self, item, quantity):
         self.item = item
         self.quantity = quantity
+        self.total = quantity * item.price
 
     def __str__(self):
-        return '{1}x {0}'.format(self.item, self.quantity)
+        return '{1}x {0}'.format(self.item, self.quantity) 
 
     def __repr__(self):
         return self.__str__()
 
+    def line_total(self):
+        return self.total
+
+
 class Receipt(object):
     def __init__(self):
-        pass    
+	self.line_items = []
+
+    def add_line_item(self, line_item):
+        self.line_items.append(line_item)
+
+    def __str__():
+        pass
+
+    def __repr__():
+        return self.__str__()
